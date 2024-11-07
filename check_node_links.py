8c475c7f103e8177f6f5060f12a5992d1a8e719c
Adam Schaal <amschaal@gmail.com>
2024-11-07 11:05:59 -0800

    add some exception handling, etc to link checking report.  Make it use live data direct from api.

--- check_node_links.py
+++ check_node_links.py
@@ -1,3 +1,4 @@
+from download_api_data import get_all_data
 from link_utils import URLChecker, extract_urls_from_html, translate_url
 import json, csv
 
@@ -16,30 +17,40 @@ def get_node_links(node):
         return []    
     
 
-def get_all_node_links(filename):
-    with open(filename) as f:
-        nodes = json.load(f)
-        print('{} nodes in file'.format(len(nodes), filename))
-        return [{'node': node, 'links': get_node_links(node)} for node in nodes]
+def get_all_node_links(api_url):
+    nodes = get_all_data(api_url)
+    return [{'node': node, 'links': get_node_links(node)} for node in nodes]
 
-def node_links_to_csv(node_links, csv_name, from_domain, to_domain=None):
+def node_links_to_csv(node_links, csv_name, domain):
     url_checker = URLChecker()
     with open(csv_name, 'w') as f:
         writer = csv.writer(f, delimiter='\t')
-        writer.writerow(['node_id', 'node_url', 'url', 'is_absolute', 'tag', 'content_type', 'status', 'valid'])
+        writer.writerow(['node_id', 'node_url', 'url', 'link text', 'is_absolute', 'tag', 'content_type', 'status', 'valid'])
         for nl in node_links:
             node = nl['node']
             for link in nl['links']:
-                url = link['url'] if link['is_absolute'] else 'https://' + from_domain + link['url']
-                if to_domain:
-                    url = translate_url(url, from_domain=from_domain, to_domain=to_domain)
+                url = link['url'] if link['is_absolute'] else 'https://' + domain + link['url']
+                # if to_domain:
+                #     url = translate_url(url, from_domain=from_domain, to_domain=to_domain)
                 print('Checking URL {}'.format(url))
-                link_status = url_checker.check_url(url) or {}
-                writer.writerow([node['id'], node['attributes']['path']['alias'], url, link['is_absolute'], link['type'], link_status.get('content_type'), link_status.get('status'), link_status.get('valid')])
+                try:
+                    page_url = 'https://'+domain+node['attributes']['path']['alias']
+                except:
+                    page_url = 'Page URL unspecified'
+                try:
+                    link_status = url_checker.check_url(url) or {}
+                    writer.writerow([node['id'], page_url, url, (link.get('text') or 'N/A')[:30], link['is_absolute'], link['type'], link_status.get('content_type'), link_status.get('status'), link_status.get('valid')])
+                except Exception as e:
+                    print('Failed to check link {}.  Exception: {}'.format(url, str(e)))
+                    writer.writerow([node['id'], page_url, url, (link.get('text') or 'N/A')[:30], None, None, None, 'Error', None])
+                
     # url_checker.close_connections()
 def main():
-    node_links = get_all_node_links('sf_page.json')
-    node_links_to_csv(node_links, 'sf_page_link_translated_test.tsv', from_domain='dnatech.genomecenter.ucdavis.edu', to_domain='dnatech.sf.ucdavis.edu')
+    content_type = 'sf_page'
+    site = json.load(open('.siteinfo.json'))
+    api_url = 'https://{}/jsonapi/node/{}'.format(site['site'], content_type)
+    node_links = get_all_node_links(api_url)
+    node_links_to_csv(node_links, '{}_links.tsv'.format(content_type), domain=site['site'])
 
 if __name__ == "__main__":
     main()
